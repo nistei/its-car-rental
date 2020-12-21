b54c5c39181d76cb53db5e826160779507428840
Niklas Steiner <niklas@sbg.at>
2020-12-21 22:56:13 +0100

    Add .eslintignore

--- scripts/secrets.js
+++ scripts/secrets.js
@@ -1,6 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const crypto = require("crypto");
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require("fs");
 
 function secret(path) {
