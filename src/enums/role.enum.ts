4d190b56d1f12ecd147141dc3600db1cc1b2af1d
Niklas Steiner <niklas@sbg.at>
2021-01-09 11:42:48 +0100

    Add roles

--- src/enums/role.enum.ts
+++ src/enums/role.enum.ts
@@ -1,6 +1,10 @@
 export enum Role {
-  User = 'user',
   Admin = 'admin',
+  FleetManager = 'fleet-manager',
+  Billing = 'billing',
   Mechanic = 'mechanic',
+  FrontOffice = 'front-office',
+  CustomerService = 'customer-service',
+  Customer = 'customer',
   Ghost = 'ghost',
 }
