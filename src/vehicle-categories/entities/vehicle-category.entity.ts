526c702674cffd64e71deac2e8956d8e61d66f5b
Niklas Steiner <niklas@sbg.at>
2021-01-16 18:46:45 +0100

    feat: add category entity properties

--- src/vehicle-categories/entities/vehicle-category.entity.ts
+++ src/vehicle-categories/entities/vehicle-category.entity.ts
@@ -1,7 +1,15 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, Index } from 'typeorm';
 import { BaseEntity } from '../../common/base.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class VehicleCategory extends BaseEntity {
+  @Column()
+  @Index()
+  @ApiProperty({ description: 'The name of the category' })
+  name: string;
 
+  @Column()
+  @ApiProperty({ description: 'URL to the icon of the category' })
+  iconUrl: string;
 }
