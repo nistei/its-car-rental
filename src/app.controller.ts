7937a01604f0e6cfea57e7a2e131d783f11d660e
Niklas Steiner <niklas@sbg.at>
2021-01-16 18:31:52 +0100

    fix: swagger ui not reachable

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AppController {
   @Get()
+  @Public()
   @Redirect('/api/#', 301)
   documentation() {
     return;
