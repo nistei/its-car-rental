152caf6af0b99f8a5016fdedcf1743965c018f57
Niklas Steiner <niklas@sbg.at>
2021-01-19 18:18:08 +0100

    fix: use email field instead of username for login

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -6,7 +6,7 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
   constructor(private authService: AuthService) {
-    super();
+    super({ usernameField: 'email' });
   }
 
   async validate(email: string, password: string): Promise<any> {
